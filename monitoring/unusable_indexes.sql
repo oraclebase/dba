9da8a9b493f5d0fc2dfc77a168297bdaf7b6f938
Tim Hall <tim@oracle-base.com>
2018-01-28 13:30:56 +0000

    Add extra columns and alter formatting.

--- monitoring/unusable_indexes.sql
+++ monitoring/unusable_indexes.sql
@@ -4,12 +4,21 @@
 -- Description  : Displays unusable indexes for the specified schema or all schemas.
 -- Requirements : Access to the DBA views.
 -- Call Syntax  : @unusable_indexes (schema-name or all)
--- Last Modified: 05/11/2004
+-- Last Modified: 28/01/2018
 -- -----------------------------------------------------------------------------------
-SET VERIFY OFF
+SET VERIFY OFF LINESIZE 200
+
+COLUMN owner FORMAT A30
+COLUMN index_name FORMAT A30
+COLUMN table_owner FORMAT A30
+COLUMN table_name FORMAT A30
 
 SELECT owner,
-       index_name
+       index_name,
+       index_type,
+       table_owner,
+       table_name
+       table_type
 FROM   dba_indexes
 WHERE  owner = DECODE(UPPER('&1'), 'ALL', owner, UPPER('&1'))
 AND    status NOT IN ('VALID', 'N/A')
