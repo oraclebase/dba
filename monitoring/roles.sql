81ef2d97dc0c5d72f0ed98f5dd52d1ea303cd764
Tim Hall <tim@oracle-base.com>
2018-02-27 16:55:14 +0000

    Format ROLE column output, limiting to 30 characters.

--- monitoring/roles.sql
+++ monitoring/roles.sql
@@ -4,10 +4,12 @@
 -- Description  : Displays a list of all roles and privileges granted to the specified user.
 -- Requirements : Access to the USER views.
 -- Call Syntax  : @roles
--- Last Modified: 15/07/2000
+-- Last Modified: 27/02/2018
 -- -----------------------------------------------------------------------------------
 SET SERVEROUTPUT ON
 SET VERIFY OFF
+
+COLUMN role FORMAT A30
 
 SELECT a.role,
        a.password_required,
