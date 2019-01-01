9f8a82cf8007d4e70b275b49c6ca3746bcab1235
Tim Hall <tim@oracle-base.com>
2019-01-01 18:56:06 +0000

    Add format for NAME column.

--- 12c/pdbs.sql
+++ 12c/pdbs.sql
@@ -4,7 +4,7 @@
 -- Description  : Displays information about all PDBs in the current CDB.
 -- Requirements : Access to the DBA views.
 -- Call Syntax  : @pdbs
--- Last Modified: 18/12/2013
+-- Last Modified: 01/01/2019 - Added format for NAME column.
 -- -----------------------------------------------------------------------------------
 
 COLUMN pdb_name FORMAT A20
@@ -12,6 +12,8 @@ COLUMN pdb_name FORMAT A20
 SELECT pdb_name, status
 FROM   dba_pdbs
 ORDER BY pdb_name;
+
+COLUMN name FORMAT A20
 
 SELECT name, open_mode
 FROM   v$pdbs
