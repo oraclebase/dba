431bdf9ab85fb18209c4a9da8a8e42c4c815228e
Tim Hall <tim@oracle-base.com>
2019-01-05 19:11:13 +0000

    Increase the LINESIZE setting and include PDB ID and name.

--- 12c/lockdown_profiles.sql
+++ 12c/lockdown_profiles.sql
@@ -4,10 +4,11 @@
 -- Description  : Displays information about lockdown profiles.
 -- Requirements : Access to the DBA views.
 -- Call Syntax  : @lockdown_profiles
--- Last Modified: 05/01/2019 - Increase the LINESIZE setting.
+-- Last Modified: 05/01/2019 - Increase the LINESIZE setting and include PDB ID and name.
 -- -----------------------------------------------------------------------------------
-SET LINESIZE 210
+SET LINESIZE 250
 
+COLUMN pdb_name FORMAT A30
 COLUMN profile_name FORMAT A30
 COLUMN rule_type FORMAT A20
 COLUMN rule FORMAT A20
@@ -18,15 +19,18 @@ COLUMN min_value FORMAT A20
 COLUMN max_value FORMAT A20
 COLUMN list FORMAT A20
 
-SELECT profile_name,
-       rule_type,
-       rule,
-       clause,
-       clause_option,
-       option_value,
-       min_value,
-       max_value,
-       list,
-       status
-FROM   dba_lockdown_profiles
-ORDER BY 1;
+SELECT lp.con_id,
+       p.pdb_name,
+       lp.profile_name,
+       lp.rule_type,
+       lp.rule,
+       lp.clause,
+       lp.clause_option,
+       lp.option_value,
+       lp.min_value,
+       lp.max_value,
+       lp.list,
+       lp.status
+FROM   cdb_lockdown_profiles lp
+       JOIN cdb_pdbs p ON p.con_id = lp.con_id
+ORDER BY 1, 3;
