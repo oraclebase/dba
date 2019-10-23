0c286f9fe164697d99cecc771e06ed2da98e2945
Tim Hall <tim@oracle-base.com>
2019-10-23 10:15:48 +0100

    Created user_objects.sql script.

---
+++ monitoring/user_objects.sql
@@ -0,0 +1,14 @@
+-- -----------------------------------------------------------------------------------
+-- File Name    : https://oracle-base.com/monitoring/user_objects.sql
+-- Author       : Tim Hall
+-- Description  : Displays the objects owned by the current user.
+-- Requirements : 
+-- Call Syntax  : @dynamic_memory
+-- Last Modified: 23-OCT-2019
+-- -----------------------------------------------------------------------------------
+
+COLUMN object_name FORMAT A30
+
+SELECT object_name, object_type
+FROM   user_objects
+ORDER BY 1, 2;
