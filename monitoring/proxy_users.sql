29194da98e313df50134741948aae8d1b6c29cb3
Tim Hall <tim@oracle-base.com>
2020-06-02 08:10:28 +0100

    Add proxy_users.sql.

---
+++ monitoring/proxy_users.sql
@@ -0,0 +1,18 @@
+-- -----------------------------------------------------------------------------------
+-- File Name    : https://oracle-base.com/dba/monitoring/proxy_users.sql
+-- Author       : Tim Hall
+-- Description  : Displays information about proxy users.
+-- Requirements : Access to the PROXY_USERS views.
+-- Call Syntax  : @proxy_users.sql {username or %}
+-- Last Modified: 02/06/2020
+-- -----------------------------------------------------------------------------------
+
+COLUMN proxy FORMAT A30
+COLUMN client FORMAT A30
+
+SELECT proxy,
+       client,
+       authentication,
+       flags
+FROM   proxy_users
+WHERE  proxy LIKE UPPER('%&1%');
