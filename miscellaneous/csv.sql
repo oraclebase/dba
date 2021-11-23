b8585a91a2edec2c11ec6eff65fbb3f843a182c2
Tim Hall <tim@oracle-base.com>
2021-11-23 17:43:16 +0000

    Add timestamp support.

--- miscellaneous/csv.sql
+++ miscellaneous/csv.sql
@@ -19,6 +19,7 @@ CREATE OR REPLACE PACKAGE csv AS
 --   31-JAN-2019  Tim Hall  Add set_quotes procedure.
 --   22-NOV-2020  Tim Hall  Amend set_quotes to allow control of string escaping.
 --   16-MAY-2021  Tim Hall  Add set_date_format procedure.
+--   23-NOV-2021  Tim Hall  Add timestamp support.
 -- --------------------------------------------------------------------------
 
 PROCEDURE generate (p_dir        IN  VARCHAR2,
@@ -36,6 +37,12 @@ PROCEDURE output_rc (p_refcursor  IN OUT SYS_REFCURSOR);
 PROCEDURE set_separator (p_sep  IN  VARCHAR2);
 
 PROCEDURE set_date_format (p_date_format  IN  VARCHAR2);
+
+PROCEDURE set_ts_format (p_ts_format  IN  VARCHAR2);
+
+PROCEDURE set_ts_ltz_format (p_ts_ltz_format  IN  VARCHAR2);
+
+PROCEDURE set_ts_tz_format (p_ts_tz_format  IN  VARCHAR2);
 
 PROCEDURE set_quotes (p_add_quotes  IN  BOOLEAN := TRUE,
                       p_quote_char  IN  VARCHAR2 := '"',
@@ -85,11 +92,15 @@ CREATE OR REPLACE PACKAGE BODY csv AS
 --                          Suggested by Anssi Kanninen.
 --   16-MAY-2021  Tim Hall  Add set_date_format procedure.
 --                          Alter generate_all to use the date format.
+--   23-NOV-2021  Tim Hall  Add timestamp support.
 -- --------------------------------------------------------------------------
 
 g_out_type         VARCHAR2(1)   := 'F';
 g_sep              VARCHAR2(5)   := ',';
 g_date_format      VARCHAR2(100) := 'yyyy-mm-dd hh24:mi:ss';
+g_ts_format        VARCHAR2(100) := 'yyyy-mm-dd hh24:mi:ss.xff';
+g_ts_ltz_format    VARCHAR2(100) := 'yyyy-mm-dd hh24:mi:ss.Xff am tzr';
+g_ts_tz_format     VARCHAR2(100) := 'yyyy-mm-dd hh24:mi:ss.Xff am tzr';
 g_add_quotes       BOOLEAN       := TRUE;
 g_quote_char       VARCHAR2(1)   := '"';
 g_escape           BOOLEAN       := TRUE;
@@ -172,9 +183,14 @@ PROCEDURE generate_all (p_dir        IN  VARCHAR2,
   l_desc_tab      DBMS_SQL.desc_tab2;
   l_buffer        VARCHAR2(32767);
   l_date          DATE;
-  l_timestamp     TIMESTAMP;
+  l_ts            TIMESTAMP;
+  l_ts_ltz        TIMESTAMP WITH LOCAL TIME ZONE;
+  l_ts_tz         TIMESTAMP WITH TIME ZONE;
   l_is_str        BOOLEAN;
   l_is_date       BOOLEAN;
+  l_is_ts         BOOLEAN;
+  l_is_ts_ltz     BOOLEAN;
+  l_is_ts_tz      BOOLEAN;
 
   l_file          UTL_FILE.file_type;
 BEGIN
@@ -190,11 +206,18 @@ BEGIN
   DBMS_SQL.describe_columns2 (l_cursor, l_col_cnt, l_desc_tab);
 
   FOR i IN 1 .. l_col_cnt LOOP
-    IF l_desc_tab(i).col_type = DBMS_TYPES.typecode_date THEN
-      DBMS_SQL.define_column(l_cursor, i, l_date);
-    ELSE
-      DBMS_SQL.define_column(l_cursor, i, l_buffer, 32767);
-    END IF;
+    CASE
+      WHEN l_desc_tab(i).col_type = DBMS_TYPES.typecode_date THEN
+        DBMS_SQL.define_column(l_cursor, i, l_date);
+      WHEN l_desc_tab(i).col_type = 180 THEN
+        DBMS_SQL.define_column(l_cursor, i, l_ts);
+      WHEN l_desc_tab(i).col_type = 231 THEN
+        DBMS_SQL.define_column(l_cursor, i, l_ts_ltz);
+      WHEN l_desc_tab(i).col_type = 181 THEN
+        DBMS_SQL.define_column(l_cursor, i, l_ts_tz);
+      ELSE
+        DBMS_SQL.define_column(l_cursor, i, l_buffer, 32767);
+    END CASE;
   END LOOP;
 
   IF p_query IS NOT NULL THEN
@@ -224,12 +247,39 @@ BEGIN
       END IF;
 
       -- Reset flags.
-      l_is_date := FALSE;
-      l_is_str := FALSE;
+      l_is_date   := FALSE;
+      l_is_ts     := FALSE;
+      l_is_ts_ltz := FALSE;
+      l_is_ts_tz  := FALSE;
+      l_is_str    := FALSE;
+
+      dbms_output.put_line('Before : ' || l_desc_tab(i).col_type);
 
       -- Check if this is a date column.
       IF l_desc_tab(i).col_type = DBMS_TYPES.typecode_date THEN
+        dbms_output.put_line('DATE : ' || l_desc_tab(i).col_type);
         l_is_date := TRUE;
+        l_is_str := TRUE;
+      END IF;
+
+      -- Check if this is a timestamp column.
+      IF l_desc_tab(i).col_type = 180  THEN
+        dbms_output.put_line('TIMESTAMP : ' || l_desc_tab(i).col_type);
+        l_is_ts := TRUE;
+        l_is_str := TRUE;
+      END IF;
+
+      -- Check if this is a timestamp with local time zone column.
+      IF l_desc_tab(i).col_type = 231  THEN
+        dbms_output.put_line('TIMESTAMP WITH LOCAL TIME ZONE: ' || l_desc_tab(i).col_type);
+        l_is_ts_ltz := TRUE;
+        l_is_str := TRUE;
+      END IF;
+
+      -- Check if this is a timestamp with time zone column.
+      IF l_desc_tab(i).col_type = 181  THEN
+        dbms_output.put_line('TIMESTAMP WITH TIME ZONE: ' || l_desc_tab(i).col_type);
+        l_is_ts_tz := TRUE;
         l_is_str := TRUE;
       END IF;
 
@@ -241,16 +291,27 @@ BEGIN
                                     DBMS_TYPES.typecode_nvarchar2,
                                     DBMS_TYPES.typecode_nchar,
                                     DBMS_TYPES.typecode_nclob) THEN
+        dbms_output.put_line('STRING : ' || l_desc_tab(i).col_type);
         l_is_str := TRUE;
       END IF;
 
       -- Get the value into the buffer in the correct format.
-      IF l_is_date THEN
-        DBMS_SQL.COLUMN_VALUE(l_cursor, i, l_date);
-        l_buffer := to_char(l_date, g_date_format);
-      ELSE
-        DBMS_SQL.COLUMN_VALUE(l_cursor, i, l_buffer);
-      END IF;
+      CASE
+        WHEN l_is_date THEN
+          DBMS_SQL.COLUMN_VALUE(l_cursor, i, l_date);
+          l_buffer := to_char(l_date, g_date_format);
+        WHEN l_is_ts THEN
+          DBMS_SQL.COLUMN_VALUE(l_cursor, i, l_ts);
+          l_buffer := to_char(l_ts, g_ts_format);
+        WHEN l_is_ts_ltz THEN
+          DBMS_SQL.COLUMN_VALUE(l_cursor, i, l_ts_ltz);
+          l_buffer := to_char(l_ts_ltz, g_ts_ltz_format);
+        WHEN l_is_ts_tz THEN
+          DBMS_SQL.COLUMN_VALUE(l_cursor, i, l_ts_tz);
+          l_buffer := to_char(l_ts_tz, g_ts_tz_format);
+        ELSE
+          DBMS_SQL.COLUMN_VALUE(l_cursor, i, l_buffer);
+      END CASE;
 
       -- Optionally add quotes for strings.
       IF g_add_quotes AND l_is_str THEN
@@ -297,6 +358,27 @@ PROCEDURE set_date_format (p_date_format  IN  VARCHAR2) AS
 BEGIN
   g_date_format := p_date_format;
 END set_date_format;
+
+
+-- Alter timestamp format from default.
+PROCEDURE set_ts_format (p_ts_format  IN  VARCHAR2) AS
+BEGIN
+  g_ts_format := p_ts_format;
+END set_ts_format;
+
+
+-- Alter timestamp with local timezone format from default.
+PROCEDURE set_ts_ltz_format (p_ts_ltz_format  IN  VARCHAR2) AS
+BEGIN
+  g_ts_ltz_format := p_ts_ltz_format;
+END set_ts_ltz_format;
+
+
+-- Alter timestamp with timezone format from default.
+PROCEDURE set_ts_tz_format (p_ts_tz_format  IN  VARCHAR2) AS
+BEGIN
+  g_ts_tz_format := p_ts_tz_format;
+END set_ts_tz_format;
 
 
 -- Alter separator from default.
